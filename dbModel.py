88b699316b2e15b707135191f8f48e18dd73e1c8
jacky308082 <jacky308082@gmail.com>
2019-07-24 13:13:03 +0800

    make it completed

--- dbModel.py
+++ dbModel.py
@@ -3,15 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gsauxupdtlrdxw:570b63e6418005cb3a9df34e84edf0044136a57b1b3705847d29a33415ff734c@ec2-54-243-208-234.compute-1.amazonaws.com:5432/d2od04jmuld6tc'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+db = SQLAlchemy()
 
 class resultData(db.Model):
 	__tablename__ = 'db'
@@ -27,4 +19,13 @@ class resultData(db.Model):
 		self.createDate = createDate
 
 if __name__ == "__main__": 
+	app = Flask(__name__)
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gsauxupdtlrdxw:570b63e6418005cb3a9df34e84edf0044136a57b1b3705847d29a33415ff734c@ec2-54-243-208-234.compute-1.amazonaws.com:5432/d2od04jmuld6tc'
+	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+	db = SQLAlchemy(app)
+	migrate = Migrate(app,db)
+
+	manager = Manager(app)
+	manager.add_command('db', MigrateCommand)
 	manager.run()
